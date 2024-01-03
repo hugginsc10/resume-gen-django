b65c687e8f13285b6b472fcbb645617da39c6de9
Chas Huggins <hugginsc10@gmail.com>
2024-01-03 17:59:09 -0500

    Added Interests, Education, Work Experience, Skills, and CV Models

--- app/CV_gen/models.py
+++ app/CV_gen/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Interests(models.Model):
+    interest = models.CharField(max_length=200)
+
+class Education(models.Model):
+    university = models.CharField(max_length=200)
+    degree = models.CharField(max_length=200)
+    gpa = models.DecimalField(max_digit=5, decimal_places=2)
+
+class WorkExperience(models.Model):
+    job_title = models.CharField(max_length=200)
+    company_name = models.CharField(max_length=200)
+    start_date = models.CharField(max_length=200)
+    end_date = models.CharField(max_length=200)
+    other_information = models.TextField(max_length=2000)
+
+class Skills(models.Model):
+    skills = models.CharField(max_length=200)
+
+class CV_data(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    bio = models.TextField(max_length=2000)
+    skills = models.ManyToManyField(Skills)
+    interests = models.ManyToManyField(Interests)
+    education = models.ManyToManyField(Education)
+    work_experience = models.ManyToManyField(WorkExperience)
+
+    
