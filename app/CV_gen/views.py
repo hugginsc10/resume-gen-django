ac2163bc4f2a326362b0288c597b589013ef5ae9
Chas Huggins <hugginsc10@gmail.com>
2024-01-03 17:51:58 -0500

    fixed path for templates

--- app/CV_gen/views.py
+++ app/CV_gen/views.py
@@ -4,11 +4,10 @@ from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
 def index(request):
-
     return render(request, "CV_gen/index.html")
 
 def cv_list(request):
-    return render(request, "CV_gen/cv_list.html")
+    return render(request, "CV_gen/list.html")
 
 def submit(request):
     pass
